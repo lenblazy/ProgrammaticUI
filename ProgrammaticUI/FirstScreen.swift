42feca0c7283194d37048c002a651eb0ea95aed0
Lennox Mwabonje <lenblazy@gmail.com>
2024-10-01 09:37:26 +0300

    Reference the first viewcontroller

--- ProgrammaticUI/FirstScreen.swift
+++ ProgrammaticUI/FirstScreen.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        debugPrint("hahahahaha")
+        view.backgroundColor = .red
     }
 
 
